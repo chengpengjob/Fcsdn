47670247a3a7064598257e741b94606169c9a9e2
chengpengjob <chenpenjob@163.com>
2019-03-19 14:45:02 +0800

    Initial commit

---
+++ src/main/java/com/cp/fcsdn/FcsdnApplication.java
@@ -0,0 +1,13 @@
+package com.cp.fcsdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcsdnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcsdnApplication.class, args);
+    }
+
+}
